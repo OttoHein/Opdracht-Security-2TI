2efa5e299f17e8ea57c4f8af82ad02e1a01b9a86
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-03-24 10:56:41 +0100

    Response splitting
    vulnerability added

--- routes/search.js
+++ routes/search.js
@@ -25,6 +25,7 @@ router.post('/', function(req, res, next) {
     ? res.render('search', {title: row.title, content: row.content, searchedfor: req.body.title})
     : res.render('search', {searchedfor: req.body.title + '(Not found)'});
   });
+  res.set('title',req.body.title);
 });
 
 module.exports = router;
